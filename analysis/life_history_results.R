f45f3b8daada1c1f88d715d43486a6c7e446d70e
Tom Miller <tom.miller@rice.edu>
2025-07-24 15:03:41 -0500

    code for new plots

--- analysis/life_history_results.R
+++ analysis/life_history_results.R
@@ -9,7 +9,7 @@ library(patchwork)
 ##read in life history outputs
 lifehistorypost<-read.csv("analysis/lifehistorypost.csv")
 ## add posterior draw -- we used 100 samples for each species
-lifehistorypost$draw<-rep(1:100,times=7)
+lifehistorypost$draw<-rep(1:n_post,times=7) #n_post from vital rates script
 
 # PCA ---------------------------------------------------------------------
 ## need to pivot trait data from wide to long
@@ -79,6 +79,12 @@ for (d in unique(pca.dat$Draw)) {
 }
 posterior_shift_df <- bind_rows(posterior_shifts)
 
+posterior_shift_means<-posterior_shift_df %>% 
+  group_by(Species) %>% 
+  summarise(mean1 = mean(dPC1),
+            mean2 = mean(dPC2))%>%
+    mutate(direction1 = ifelse(mean1 >= 0, "positive", "negative"),
+           direction2 = ifelse(mean2 >= 0, "positive", "negative"))
 
 ##Figure of shift in PC space
 scores_df <- as_tibble(pca_ref$x[, 1:2], .name_repair = "unique") %>%
@@ -127,8 +133,9 @@ ggplot() +
        x = "PC1", y = "PC2", color = "Symbiont status")->PCA_plot
 
 ## Figure of posterior shifts
+lims1 <- quantile(posterior_shift_df$dPC1, probs = c(0.01, 0.99))
 ggplot(posterior_shift_df, aes(x = dPC1, y = Species, fill = stat(x))) + 
-  geom_density_ridges_gradient(scale = 2, rel_min_height = 0.01, color = "black", size = 0.3) +
+  geom_density_ridges_gradient(scale = 1, rel_min_height = 0.01, color = "black", size = 0.3) +
   scale_fill_gradient2(
     low = "tomato",
     mid = "white",
@@ -137,11 +144,24 @@ ggplot(posterior_shift_df, aes(x = dPC1, y = Species, fill = stat(x))) +
     limits = c(-5, 5),  # clip values outside this range
     oob = scales::squish,  # squish outliers into endpoint colors
     name = "Shift"
-  ) +  theme_minimal(base_size = 13) + theme(legend.position = "none") +
+  ) +  coord_cartesian(xlim = lims1) + 
+  theme_minimal(base_size = 13) + theme(legend.position = "none") +
   labs(title = "B)",
-       x = expression(Delta~PC1), y = "Species")+ geom_vline(xintercept = 0) -> PC1_plot
+       x = expression(Delta~PC1), y = "Species")+ geom_vline(xintercept = 0)+
+  geom_point(
+    data = posterior_shift_means,
+    aes(x = mean1, y = Species, color = direction1),  # now fill is mapped!
+    inherit.aes = FALSE,
+    shape = 16,
+    size = 2
+  ) +
+  scale_color_manual(
+    values = c("positive" = "cornflowerblue", "negative" = "tomato")
+  )-> PC1_plot
+
+lims2 <- quantile(posterior_shift_df$dPC2, probs = c(0.01, 0.99))
 ggplot(posterior_shift_df, aes(x = dPC2, y = Species, fill = stat(x))) + 
-  geom_density_ridges_gradient(scale = 2, rel_min_height = 0.01, color = "black", size = 0.3) +
+  geom_density_ridges_gradient(scale = 1, rel_min_height = 0.01, color = "black", size = 0.3) +
   scale_fill_gradient2(
     low = "tomato",
     mid = "white",
@@ -150,9 +170,20 @@ ggplot(posterior_shift_df, aes(x = dPC2, y = Species, fill = stat(x))) +
     limits = c(-5, 5),  # clip values outside this range
     oob = scales::squish,  # squish outliers into endpoint colors
     name = "Shift"
-  ) +  theme_minimal(base_size = 13) + theme(legend.position = "none") +
+  ) + coord_cartesian(xlim = lims1) + 
+  theme_minimal(base_size = 13) + theme(legend.position = "none") +
   labs(title = "C)",
-       x = expression(Delta~PC2), y = "Species")+ geom_vline(xintercept = 0) -> PC2_plot
+       x = expression(Delta~PC2), y = "Species")+ geom_vline(xintercept = 0)+
+  geom_point(
+    data = posterior_shift_means,
+    aes(x = mean2, y = Species, color = direction2),  # now fill is mapped!
+    inherit.aes = FALSE,
+    shape = 16,
+    size = 2
+  ) +
+  scale_color_manual(
+    values = c("positive" = "cornflowerblue", "negative" = "tomato")
+  )-> PC2_plot
 
 PCA_combo <- PCA_plot / (PC1_plot | PC2_plot)
 
@@ -179,8 +210,13 @@ stat_density_ridges(
     x = "Shift in PC1", y = "Species"
   ) + geom_vline(xintercept = 0)
 
-
-PC1_plot
+## mean shift and probability of PC shift
+posterior_shift_df %>% 
+  group_by(Species) %>% 
+  summarise(mean_shift1=mean(dPC1),
+            mean_shift2=mean(dPC2),
+            prob_shift1=mean(dPC1>0),
+            prob_shift2=mean(dPC2>0))
 
 groups<-as.factor(mean.pca.dat$Endo)
 fviz_pca_biplot(pca_ref, repel = TRUE,
@@ -239,19 +275,118 @@ lifehistorypost$gini_diff<-lifehistorypost$gini_ep-lifehistorypost$gini_em
 lifehistorypost$longevity_diff<-lifehistorypost$longevity_ep-lifehistorypost$longevity_em
 lifehistorypost$firstrepro_diff<-lifehistorypost$firstrepro_ep-lifehistorypost$firstrepro_em
 
-## some quick and dirty plots of life history effects
+## data frame for life history effects
 lifehistorypost %>% 
   group_by(species) %>% 
-  summarise(pr_epos_lambda = mean(lambda_diff>0),
-            pr_epos_R0 = mean(R0_diff>0),
-            pr_epos_G = mean(G_diff>0),
-            pr_epos_meanelexp = mean(meanelexp_diff>0),
-            pr_epos_longevity = mean(longevity_diff>0))
+  summarise(`R0` = mean(R0_diff>0),
+            `Generation time` = mean(G_diff>0),
+            `Life expectancy` = mean(meanelexp_diff>0),
+            `Longevity` = mean(longevity_diff>0),
+            `Entropy` = mean(entropyd_diff>0),
+            `Reproductive age` = mean(firstrepro_diff>0))->life_history_effects
+
+life_history_effects %>%
+  pivot_longer(cols = -species, names_to = "metric", values_to = "probpos") %>% 
+  ggplot(aes(x = species, y = metric, fill = probpos)) +
+  geom_tile() +
+  scale_fill_gradientn(colors=c("tomato", "grey", "cornflowerblue")) + 
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(fill = "S+ Adv.e", x = "Species", y = "Metric", title = "Heatmap of Life History Metrics")
+
+life_history_effects %>%
+  pivot_longer(cols = -species, names_to = "metric", values_to = "probpos") %>% 
+  ggplot(aes(x = species, y = metric, fill = probpos)) +
+  geom_tile() +
+  geom_text(aes(label = sprintf("%.2f", probpos)), size = 3) +  # Add values to each tile
+  scale_fill_gradient2(
+    low = "tomato",
+    mid = "grey",
+    high = "cornflowerblue",
+    midpoint = 0.5,
+    limits = c(0, 1),
+    name = "Probability of\nS+ advantage"
+  ) + 
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Species", y = "Life history trait") -> LHtraits_heatmap
+
+ggsave("manuscript/figures/LHtraits_heatmap.jpg",
+       plot = LHtraits_heatmap,
+       width = 10,      # width in inches
+       height = 8,      # height in inches
+       dpi = 300,       # resolution
+       units = "in")
+
+## effects on net fitness (lambda)
 
 ##lambda
 ggplot(lifehistorypost,aes(lambda_diff,fill=species,col=species))+
   geom_density(alpha=0.1)+geom_vline(xintercept=0)+
   facet_wrap(~species,scales="free")
+
+
+lifehistorypost %>% 
+  group_by(species) %>% 
+  summarise(mean_diff = mean(lambda_diff),
+            prop_positive = mean(lambda_diff>0),
+            prop_negative = mean(lambda_diff<0),
+            x_pos = quantile(lambda_diff[lambda_diff > 0], 0.7, na.rm = TRUE),  
+            x_neg = quantile(lambda_diff[lambda_diff < 0], 0.3, na.rm = TRUE)) %>%
+              mutate(direction = ifelse(mean_diff >= 0, "positive", "negative"))->proportions_df
+
+lambdadiffplot<-ggplot(lifehistorypost, aes(x = lambda_diff, y = species, fill = stat(x))) + 
+  geom_density_ridges_gradient(scale = 1, rel_min_height = 0.01, color = "black", size = 0.3) +
+  scale_fill_gradient2(
+    low = "tomato",
+    mid = "white",
+    high = "cornflowerblue",
+    midpoint = 0,
+    limits = c(-.65, .65),  # clip values outside this range
+    oob = scales::squish,  # squish outliers into endpoint colors
+    name = "Shift"
+  ) +  
+  geom_text(
+    data = proportions_df,
+    aes(
+      x = x_neg,
+      y = as.numeric(factor(species)) + 0.13,  # position just below the ridge
+      label = paste0(round(prop_negative, 2))
+    ),
+    inherit.aes = FALSE,
+    size = 3
+  ) +
+  
+  # Add positive side (right tail) proportions
+  geom_text(
+    data = proportions_df,
+    aes(
+      x = x_pos,
+      y = as.numeric(factor(species)) + 0.13,  # same vertical offset
+      label = paste0(round(prop_positive, 2))
+    ),
+    inherit.aes = FALSE,
+    size = 3
+  ) +
+  theme_minimal(base_size = 13) + theme(legend.position = "none") +
+  labs(x = expression("Endophyte effect on host fitness ("*Delta~lambda*")"), y = "Species")+ geom_vline(xintercept = 0)+ 
+  geom_point(
+    data = proportions_df,
+    aes(x = mean_diff, y = species, color = direction),  # now fill is mapped!
+    inherit.aes = FALSE,
+    shape = 16,
+    size = 4
+  ) +
+  scale_color_manual(
+    values = c("positive" = "cornflowerblue", "negative" = "tomato")
+  )
+
+ggsave("manuscript/figures/lambdadiffplot.jpg",
+       plot = lambdadiffplot,
+       width = 6,      # width in inches
+       height = 8,      # height in inches
+       dpi = 300,       # resolution
+       units = "in")
 
 ##R0
 ggplot(lifehistorypost,aes(R0_diff,fill=species,col=species))+
